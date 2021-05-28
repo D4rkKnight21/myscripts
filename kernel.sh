7ee08e3efb8cb18be456c42aa5a02a3eab13291a
Panchajanya1999 <panchajanya@azure-dev.live>
2021-05-28 13:06:42 +0530

    kernel: Use zipsigner 4.0
    Signed-off-by: Panchajanya1999 <panchajanya@azure-dev.live>

--- kernel.sh
+++ kernel.sh
@@ -347,8 +347,8 @@ gen_zip() {
  			msg "|| Signing Zip ||"
 			tg_post_msg "<code>Signing Zip file with AOSP keys..</code>"
  		fi
-		curl -sLo zipsigner-3.0.jar https://raw.githubusercontent.com/baalajimaestro/AnyKernel2/master/zipsigner-3.0.jar
-		java -jar zipsigner-3.0.jar "$ZIP_FINAL".zip "$ZIP_FINAL"-signed.zip
+		curl -sLo zipsigner-4.0.jar https://github.com/baalajimaestro/AnyKernel3/raw/master/zipsigner-4.0.jar
+		java -jar zipsigner-4.0.jar "$ZIP_FINAL".zip "$ZIP_FINAL"-signed.zip
 		ZIP_FINAL="$ZIP_FINAL-signed"
 	fi
 
